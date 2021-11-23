3e12f491d397c7f1dc831d3ef0445a41c2129013
cristian <cristian09061997@gmail.com>
2021-11-23 12:34:25 -0800

    concatenacion de python

---
+++ origin.py
@@ -0,0 +1,16 @@
+name='cristian eduardo hernandez'
+age=24
+istall=True
+height=1.77
+
+print (name+" tiene "+str(age)+"años")
+print("{} tiene {} años".format(name,age))
+
+print(f"{name} tiene {age+24} años y es \
+alto {istall} y mide {height}")
+
+print(name.upper())
+print(name.lower())
+print(name.tittle())
+
+print(name*5)
